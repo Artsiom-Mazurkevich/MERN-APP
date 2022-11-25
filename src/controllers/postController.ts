763c1a35c660d38526a05ffa5b563d5c51282722
Artsiom-Mazurkevich <patrol1995@yandex.ru>
2022-11-25 13:16:03 +0300

    add delete post method

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -27,10 +27,20 @@ class PostController_ {
     }
     async deletePost (req:Request, res: Response) {
         try {
-
+            const postId = req.params.id;
+            Post.findOneAndDelete({_id: postId}, {}, (err, doc) => {
+                if (err) {
+                    console.log(err)
+                    return res.status(500).json({message: "Failed to delete article"})
+                }
+                if (!doc) {
+                    return res.status(404).json({message: "Article not found"})
+                }
+                return res.json({message: "Article deleted successfully"})
+            })
         }
         catch (e) {
-
+            console.log(e)
         }
     }
 }
--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -6,6 +6,7 @@ import {postController} from "../controllers/postController";
 const router = express.Router()
 
 router.post('/post', authMiddleware, postController.createPost)
+router.delete('/post/:id', authMiddleware, postController.deletePost)
 
 
 
